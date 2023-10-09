49f21a3278b536b4c8ed5b9c3956becb8b201ee2
nick <narendra.singh@vega-hr.com>
2023-10-09 11:43:55 +0530

    able to make from frontedn to backend

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,31 +2,48 @@ import React from "react";
 import styles from "./login.module.css";
 import useAuthService from "../../hooks/useAuthService";
 import { useNavigate } from "react-router-dom";
+import { login } from "../../services/apiServices";
+import axios from "axios";
 
 function Login() {
-  const navigate = useNavigate();
-  const authService = useAuthService();
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    authService.setUserDetails(true);
-    navigate("/chat");
-  };
-  return (
-    <div className={styles.mainBackground}>
-      <div className={styles.background}></div>
-      <form onSubmit={handleSubmit}>
-        <h3>Login Here</h3>
+	const navigate = useNavigate();
+	const authService = useAuthService();
+	const handleSubmit = async (event: any) => {
+		try {
+			event.preventDefault();
+			const payload = {
+				email: "recs.cse1626@gmail.com",
+				password: "Nerd@0604",
+			};
+			//const user = await login(payload);
+			const user = await axios({
+				method: "post",
+				url: "http://localhost:8086/v1/login/basic",
+				data: payload,
+			});
+			console.log("user", user);
+			authService.setUserDetails(true);
+			navigate("/chat");
+		} catch (error) {
+			console.log("error in handle submit", error);
+		}
+	};
+	return (
+		<div className={styles.mainBackground}>
+			<div className={styles.background}></div>
+			<form onSubmit={handleSubmit}>
+				<h3>Login Here</h3>
 
-        <label htmlFor="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username" />
+				<label htmlFor="username">Username</label>
+				<input type="text" placeholder="Email or Phone" id="username" />
 
-        <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Password" id="password" />
+				<label htmlFor="password">Password</label>
+				<input type="password" placeholder="Password" id="password" />
 
-        <button type="submit">Log In</button>
-      </form>
-    </div>
-  );
+				<button type="submit">Log In</button>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -2,27 +2,17 @@ import axios from 'axios';
 import config from '../config/config';
 import constants from '../constant/constant';
 
-let axiosInstance : any;
 
-export const getAxiosInstance = async (data:any) => {
-  axiosInstance = axios.create({
-    baseURL: `${config.app.baseUrl}`,
-    timeout: config.app.timeout,
-  });
+const axiosInstance = axios.create({
+  baseURL: `${config.app.baseUrl}/v1`,
+});
 
-  axiosInstance.interceptors.request.use((req:any) => {
-    const token = localStorage.getItem(constants.access_token);
-    req.headers.Authorization = `Bearer ${token}`;
-    return req;
-  });
-};
+axiosInstance.interceptors.request.use((req:any) => {
+  const token = localStorage.getItem(constants.access_token);
+  req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
 export const login = async (data:any) => {
   const res = await axiosInstance.post(`/basic/login`,data);
   return res && res.data ? res.data : null;
 };
-
-// export const addPay = async data => {
-//   const path = `/${UrlEndPoints.addPayment}`;
-//   const res = await paymentInstance.post(path, data);
-//   return res && res.data ? res.data : null;
-// };
